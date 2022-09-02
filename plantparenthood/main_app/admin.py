9277fbef70751ecadc4de51ad045000a281241fc
gabeantonio <antoniogabe99@gmail.com>
2022-09-02 15:44:23 -0700

    login works

--- plantparenthood/main_app/admin.py
+++ plantparenthood/main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Plant
 
 # Register your models here.
+
+admin.site.register(Plant)
+
--- plantparenthood/plantparenthood/settings.py
+++ plantparenthood/plantparenthood/settings.py
@@ -117,8 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# LOGIN_REDIRECT_URL = '/plants/'
-# LOGOUT_REDIRECT_URL = '/'
+
+LOGIN_REDIRECT_URL = '/plants/'
+
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
