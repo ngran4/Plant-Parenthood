ce0946f20264f4223b238841e576df213385fdbd
joyceyyuan <yueyuan@hotmail.com>
2022-09-02 16:06:58 -0700

    sign up new user complete

--- plantparenthood/main_app/urls.py
+++ plantparenthood/main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('plants/', views.plants_index, name='index'),
+    path('accounts/signup/', views.signup, name='signup'),
 
 ]
