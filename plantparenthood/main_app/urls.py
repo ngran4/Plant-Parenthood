45ff737a8ce56f7dcb835f65a0f8e9a145386571
ngran4 <nicgranville@gmail.com>
2022-09-03 09:47:46 -0700

    update/delete functionality

--- plantparenthood/main_app/urls.py
+++ plantparenthood/main_app/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     # Plant stuff
     path('plants/', views.plants_index, name='index'),
     path('plants/create/', views.PlantCreate.as_view(), name='plants_create'),
+    path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name="plants_update"),
+    path('plants/<int:pk>/delete/', views.PlantDelete.as_view(), name="plants_delete"),
     # Account stuff
     path('accounts/signup/', views.signup, name='signup'),
-
 ]
