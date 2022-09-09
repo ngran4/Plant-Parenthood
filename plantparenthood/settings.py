b9bf8a41d6c52b3594a1e940c64724fd4889d923
ngran4 <nicgranville@gmail.com>
2022-09-09 09:10:18 -0700

    secret key

--- plantparenthood/settings.py
+++ plantparenthood/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'v#nge!-v27dis6s^ai_^3&@g(t1qz8iijho70n*%0e6ubz9*=i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
