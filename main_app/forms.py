919ed63450b9ff1fe0042fb2a36f71c9e1cee6e8
joyceyyuan <yueyuan@hotmail.com>
2022-09-11 14:48:40 -0700

    add datepicker

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,17 @@
-from django.forms import ModelForm
+from django import forms
+
+from django.forms import ModelForm    
+from django.forms.widgets import DateInput
+
 from .models import Watering
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class WateringForm(ModelForm):
   class Meta:
     model = Watering
     fields = ['date']
+    widgets = {
+      'date': DateInput(attrs={'type': 'date'}),
+    }
